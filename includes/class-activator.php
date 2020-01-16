fea67d9826cd67ab41f88c7151e58086c192fed0
Jacob DeBenedetto <deben3@gmail.com>
2020-01-15 21:23:41 -0700

    update todo list

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -2,14 +2,16 @@
 
 class Doppler_Locator_Activator {
 	public static function activate() {
-		global $wpdb, $doppler_locator_table_name;
+        // TODO: Check to see if any posts with post_type = "template" and generate defaults posts if it does not exist
+
+
+        /*
+        global $wpdb, $doppler_locator_table_name;
 		$charset_collate = $wpdb->get_charset_collate();
 		$table_name = $doppler_locator_table_name;
 
-        // Check to see if the table exists already, if not, then create it
         if ($wpdb->get_var("SHOW TABLES LIKE '" . $table_name . "'") != $table_name) 
         {
-            // Create name and types columns for the first time
 			$sql = "CREATE TABLE " . $table_name . " 
 			( 
                 id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -17,11 +19,10 @@ class Doppler_Locator_Activator {
                 points INT(6) NOT NULL,
                 PRIMARY KEY (id)
 			) " . $charset_collate . ";";
-			
-			//echo ($sql); die;
 
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
             dbDelta($sql);
-		}
+        }
+        */
 	}
 }
--- includes/class-uninstaller.php
+++ includes/class-uninstaller.php
@@ -2,9 +2,15 @@
 
 class Doppler_Locator_Uninstaller {
 	public static function uninstall() {
+		// TODO: Delete all posts with post_type = "location" and "template"
+		// TODO: Delete all post media with "doppler-locator" in description
+
+
 		// drop a custom database table
+		/*
 		global $wpdb, $doppler_locator_table_name;
 		$table_name = $doppler_locator_table_name;
 		$wpdb->query("DROP TABLE IF EXISTS " . $table_name);
+		*/
 	}
 }

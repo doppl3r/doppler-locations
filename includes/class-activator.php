a2071c09794703ae8b94bcd10a73e96025f87700
Jacob DeBenedetto <deben3@gmail.com>
2020-01-16 18:16:50 -0700

    update post type values

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -3,19 +3,20 @@
 class Doppler_Locator_Activator {
 	public static function activate() {
         // Initialize post query
+        global $wpdb;
         $post_type = 'template';
-        $args = array('numberposts' => -1, 'post_type' => $post_type);
-        $template_count = count(get_posts($args));
+        $results = $wpdb->get_results( 
+            $wpdb->prepare("
+                SELECT post_type
+                    FROM wp_posts
+                    WHERE post_type = %s
+                ", 
+                $post_type
+            ) 
+        );
 
-        // Register post types
-        $post_types = array('location', 'template');
-        foreach($post_types as $post_type) {
-            register_post_type($post_type);
-        }
-
-        // Check if no post_type "template" exists
-        if ($template_count <= 0) {
-            // Add new post with post_type "template"
+        // Check if any templates exist
+        if (count($results) <= 0) {
             $json = file_get_contents(plugin_dir_path(dirname(__FILE__)) . 'admin/assets/default-template.json');
             $default = json_decode($json, true);
             $postarr = array(
@@ -26,8 +27,5 @@ class Doppler_Locator_Activator {
             );
             wp_insert_post($postarr);
         }
-
-        // Clear the permalinks after the post type has been registered
-        flush_rewrite_rules();
-	}
+    }
 }
--- includes/class-uninstaller.php
+++ includes/class-uninstaller.php
@@ -2,16 +2,23 @@
 
 class Doppler_Locator_Uninstaller {
 	public static function uninstall() {
-		// TODO: Delete all posts with post_type = "location" and "template"
-		// TODO: unregister post_type "location" and "template
-		// TODO: Delete all post media with "doppler-locator" in description
+		// Declare variables
+		global $wpdb;
+		$post_types = array('location', 'template');
 
-
-		// drop a custom database table
-		/*
-		global $wpdb, $doppler_locator_table_name;
-		$table_name = $doppler_locator_table_name;
-		$wpdb->query("DROP TABLE IF EXISTS " . $table_name);
-		*/
+		// Delete all posts with post_type "location" or "template"
+		foreach($post_types as $post_type) {
+			$result = $wpdb->query( 
+				$wpdb->prepare("
+					DELETE posts, terms, meta
+					FROM wp_posts posts
+					LEFT JOIN wp_term_relationships terms ON terms.object_id = posts.ID
+					LEFT JOIN wp_postmeta meta ON meta.post_id = posts.ID
+					WHERE posts.post_type = %s
+					",
+					$post_type
+				) 
+			);
+		}
 	}
 }

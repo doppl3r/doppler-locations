66f7aec02049ebc99f07fed9efbfeafb64955cfe
Jacob DeBenedetto <deben3@gmail.com>
2020-01-17 08:37:49 -0700

    add container styling

--- admin/assets/php/locations.php
+++ admin/assets/php/locations.php
@@ -1,10 +1,19 @@
-<div class="doppler-locator-body">
-    <div class="row">
+<div class="doppler-body">
+    <div class="nav row">
         <div class="col-6">
             <h1>Locations</h1>
         </div>
         <div class="col-6">
             <a class="btn" href="#">Add</a>
         </div>
+    </div>
+    <div class="container">
+        <label>Locations</label>
+        <div class="row">
+            <label class="small col-3">Status</label>
+            <label class="small col-6">Page Title</label>
+            <label class="small col-3">Action</label>
+        </div>
+        <a class="btn" href="">Add New Location</a>
     </div>
 </div>

4c8c653e4a57c795d5349383458e84a62d8d06ec
Jacob DeBenedetto <deben3@gmail.com>
2020-01-17 19:21:59 -0700

    remove debug file

--- admin/assets/php/debug.php
+++
@@ -1,8 +0,0 @@
-<div class="row" id="146">
-    <div class="col-3-m"><label class="small">Open</label></div>
-    <div class="col-6-m">Location</div>
-    <div class="col-3-m">
-        <label class="small"><a href="#edit">Edit</a></label>
-        <label class="small"><a href="#delete">Delete</a></label>
-    </div>
-</div>

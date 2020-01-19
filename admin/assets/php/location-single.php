d35b50e111a2cc36b7e889d532b2b556521e159d
doppl3r <deben3@gmail.com>
2020-01-19 09:48:23 -0700

    add default hours

--- admin/assets/php/location-single.php
+++ admin/assets/php/location-single.php
@@ -48,14 +48,22 @@
                                 <label>Sun</label>
                             </div>
                             <div class="col-4">
-                                <select name="hours-open" id="hours-open">
-                                    <?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?>
-                                </select>
+                                <select name="mon-open" id="mon-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="tue-open" id="tue-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="wed-open" id="wed-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="thu-open" id="thu-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="fri-open" id="fri-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="sat-open" id="sat-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="sun-open" id="sun-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
                             </div>
                             <div class="col-4">
-                                <select name="hours-close" id="hours-close">
-                                    <?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?>
-                                </select>
+                                <select name="mon-close" id="mon-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="tue-close" id="tue-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="wed-close" id="wed-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="thu-close" id="thu-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="fri-close" id="fri-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="sat-close" id="sat-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
+                                <select name="sun-close" id="sun-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
                             </div>
                         </div>
                     </div>
@@ -81,7 +89,29 @@
                     </div>
                     <div class="col-6 phone">
                         <label for="phone">Phone</label>
-                        <input name="phone" id="phone" type="text">
+                        <input name="phone" id="phone" type="tel">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <label for="street">Street</label>
+                        <input name="street" id="street" type="text">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <label for="latitude">Latitude</label>
+                        <input name="latitude" id="latitude" type="text">
+                    </div>
+                    <div class="col-6">
+                        <label for="longitude">Longitude</label>
+                        <input name="longitude" id="longitude" type="text">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <label for="guide">Guide</label>
+                        <input name="guide" id="guide" type="text">
                     </div>
                 </div>
             </div>

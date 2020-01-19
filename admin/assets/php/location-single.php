4a6d561f7f224d61a1706ec04a632ebed765b202
doppl3r <deben3@gmail.com>
2020-01-19 11:14:18 -0700

    style container

--- admin/assets/php/location-single.php
+++ admin/assets/php/location-single.php
@@ -36,35 +36,46 @@
                 <div class="row">
                     <div class="col-12 hours">
                         <label>Hours</label>
+                        <?php // TODO: Loop through each day and select from database ?>
                         <div class="row">
-                            <div class="col-4">
-                                <label class="small">Day</label>
-                                <label>Mon</label>
-                                <label>Tue</label>
-                                <label>Wed</label>
-                                <label>Thu</label>
-                                <label>Fri</label>
-                                <label>Sat</label>
-                                <label>Sun</label>
-                            </div>
-                            <div class="col-4">
-                                <select name="mon-open" id="mon-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="tue-open" id="tue-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="wed-open" id="wed-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="thu-open" id="thu-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="fri-open" id="fri-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="sat-open" id="sat-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="sun-open" id="sun-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                            </div>
-                            <div class="col-4">
-                                <select name="mon-close" id="mon-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="tue-close" id="tue-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="wed-close" id="wed-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="thu-close" id="thu-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="fri-close" id="fri-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="sat-close" id="sat-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                                <select name="sun-close" id="sun-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select>
-                            </div>
+                            <div class="col-2-m"><label class="small">Day</label></div>
+                            <div class="col-5-m"><label class="small">Open</label></div>
+                            <div class="col-5-m"><label class="small">Close</label></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Mon</label></div>
+                            <div class="col-5-m"><select name="mon-open" id="mon-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="mon-close" id="mon-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Tue</label></div>
+                            <div class="col-5-m"><select name="tue-open" id="tuev-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="tue-close" id="tue-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Wed</label></div>
+                            <div class="col-5-m"><select name="wed-open" id="wed-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="wed-close" id="wed-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Thu</label></div>
+                            <div class="col-5-m"><select name="thu-open" id="thu-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="thu-close" id="thu-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Fri</label></div>
+                            <div class="col-5-m"><select name="fri-open" id="fri-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="fri-close" id="fri-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Sat</label></div>
+                            <div class="col-5-m"><select name="sat-open" id="sat-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="sat-close" id="sat-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-2-m"><label>Sun</label></div>
+                            <div class="col-5-m"><select name="sun-open" id="sun-open"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
+                            <div class="col-5-m"><select name="sun-close" id="sun-close"><?php require(plugin_dir_path(dirname(__FILE__)) . 'php/hours.php'); ?></select></div>
                         </div>
                     </div>
                 </div>

ad05794691d37d21b287bf9eafdfa54d20b122b2
Jacob DeBenedetto <deben3@gmail.com>
2020-01-18 19:53:31 -0700

    update admin URL for ajax calls

--- admin/assets/php/template-row.php
+++ admin/assets/php/template-row.php
@@ -1,13 +1,13 @@
 <?php 
     /* Required variables: $row (query results) */
     $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-    $url_full = $url . "$_SERVER[REQUEST_URI]";
+    $url_edit = site_url() . '/wp-admin/admin.php?page=doppler-locator-template';
 ?>
 <div class="row" data-post="<?php echo $row->ID; ?>">
     <div class="col-3-m title"><?php echo $row->post_title; ?></div>
     <div class="col-6-m description"><?php echo $row->post_excerpt; ?></div>
     <div class="col-3-m options">
-        <label class="small"><a href="<?php echo $url_full; ?>&id=<?php echo $row->ID; ?>">Edit</a></label>
+        <label class="small"><a href="<?php echo $url_edit; ?>&id=<?php echo $row->ID; ?>">Edit</a></label>
         <label class="small"><a href="#delete">Delete</a></label>
     </div>
 </div>

bd148ffb33b0ed4c64e4ade221f3bacda10d6c60
Jacob DeBenedetto <deben3@gmail.com>
2020-01-23 09:34:59 -0700

    add leaflet library for map

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -6,85 +6,90 @@
         $data = $atts['data'];
         $type = $atts['type'];
         $id = $atts['id'];
+        $output = '';
 
         //var_dump($post_meta); die;
 
-        switch ($data) {
-            case 'title': return get_the_title($post_id); break;
-            case 'hours':
-                $output = '';
-                $hours = json_decode($post_meta['hours'][0]);
-                foreach($hours as $key=>$day) {
-                    $output .= '<li><span>'. $key. '</span> <span>' . $hours->$key . '</span></li>';
-                }
-                $output = '<ul>' . $output . '</ul>';
-                return $output;
-            break;
-            case 'media':
-                $output = '';
-                $media = json_decode($post_meta['media'][0]);
-                foreach($media as $medium) {
-                    $medium_id = $medium->id;
-                    $medium_post_id = $medium->post_id;
-                    $medium_url = wp_get_attachment_url($medium_post_id);
-                    $medium_title = get_the_title($medium_post_id);
-                    $medium_alt = get_post_meta($medium_post_id, '_wp_attachment_image_alt')[0];
-                    $medium_type = explode("/", get_post_mime_type($medium_post_id))[0];
-                    
-                    // Loop through all assets or by group id
-                    if (!isset($id) || $id == $medium_id) {
-                        switch ($medium_type) {
-                            case "image": $output .= '<img alt="' . $medium_alt . '" src="' . $medium_url . '" />'; break;
-                            case "audio": $output .= '<audio src="' . $medium_url . '" controls>' . $medium_title . '</audio>'; break;
-                            case "video": $output .= '<video src="' . $medium_url . '" controls>' . $medium_title . '</video>'; break;
-                            default: $output .= '<a href="' . $medium_url . '" target="_blank">' . $medium_title . '</a>'; break;
-                        }
+        if ($data == 'title') { return get_the_title($post_id); }
+        else if ($data == 'hours') {
+            $hours = json_decode($post_meta['hours'][0]);
+            foreach($hours as $key=>$day) {
+                $output .= '<li><span>'. $key. '</span> <span>' . $hours->$key . '</span></li>';
+            }
+            $output = '<ul>' . $output . '</ul>';
+            return $output;
+        }
+        else if ($data == 'media') {
+            $media = json_decode($post_meta['media'][0]);
+            foreach($media as $medium) {
+                $medium_id = $medium->id;
+                $medium_post_id = $medium->post_id;
+                $medium_url = wp_get_attachment_url($medium_post_id);
+                $medium_title = get_the_title($medium_post_id);
+                $medium_alt = get_post_meta($medium_post_id, '_wp_attachment_image_alt')[0];
+                $medium_type = explode("/", get_post_mime_type($medium_post_id))[0];
+                
+                // Loop through all assets or by group id
+                if (!isset($id) || $id == $medium_id) {
+                    switch ($medium_type) {
+                        case "image": $output .= '<img alt="' . $medium_alt . '" src="' . $medium_url . '" />'; break;
+                        case "audio": $output .= '<audio src="' . $medium_url . '" controls>' . $medium_title . '</audio>'; break;
+                        case "video": $output .= '<video src="' . $medium_url . '" controls>' . $medium_title . '</video>'; break;
+                        default: $output .= '<a href="' . $medium_url . '" target="_blank">' . $medium_title . '</a>'; break;
                     }
                 }
-                return $output;
-            break;
-            case 'posts':
-                $output = '';
-                $posts = json_decode($post_meta['custom_posts'][0]);
-                foreach($posts as $post) {
-                    $post_type = $post->type;
-                    $post_title = $post->title;
-                    $post_date = $post->date;
-                    $post_link = $post->link;
-                    $post_content = $post->content;
-                    
-                    if (!isset($type) || $type == $post_type) {
-                        $output .= '
-                            <ul class="row '. $post_type .'">
-                                <li class="col">' . $post_title . '</li>
-                                <li class="col">' . $post_date . '</li>
-                                <li class="col">' . $post_link . '</li>
-                                <li class="col">' . $post_content . '</li>
-                            </ul>
-                        ';
-                    }
+            }
+            return $output;
+        }
+        else if ($data == 'posts') {
+            $posts = json_decode($post_meta['custom_posts'][0]);
+            foreach($posts as $post) {
+                $post_type = $post->type;
+                $post_title = $post->title;
+                $post_date = $post->date;
+                $post_link = $post->link;
+                $post_content = $post->content;
+                
+                if (!isset($type) || $type == $post_type) {
+                    $output .= '
+                        <ul class="row '. $post_type .'">
+                            <li class="col">' . $post_title . '</li>
+                            <li class="col">' . $post_date . '</li>
+                            <li class="col">' . $post_link . '</li>
+                            <li class="col">' . $post_content . '</li>
+                        </ul>
+                    ';
                 }
-                return $output;
-            break;
-            case 'links':
-                $output = '';
-                $links = json_decode($post_meta['links'][0]);
-                foreach($links as $link) {
-                    $link_text = $link->text;
-                    $link_url = $link->url;
-                    $link_target = $link->target;
-                    $link_id = $link->id;
-                    
-                    if (!isset($id) || $id == $link_id) {
-                        $output .= '<a href="'. $link_url .'" target="' . $link_target . '">' . $link_text . '</a>';
-                    }
+            }
+            return $output;
+        }
+        else if ($data == 'links') {
+            $links = json_decode($post_meta['links'][0]);
+            foreach($links as $link) {
+                $link_text = $link->text;
+                $link_url = $link->url;
+                $link_target = $link->target;
+                $link_id = $link->id;
+                
+                if (!isset($id) || $id == $link_id) {
+                    $output .= '<a href="'. $link_url .'" target="' . $link_target . '">' . $link_text . '</a>';
                 }
-                return $output;
-            break;
-            case 'map':
-                $output = '';
-                return $output; break;
-            default: return $post_meta[$data][0]; break;
+            }
+            return $output;
+        }
+        else if ($data == 'map') {
+            global $include_leaflet;
+            $include_leaflet = true;
+
+            // Enqueue data if map exists
+            wp_enqueue_style('leaflet');
+            wp_enqueue_script('leaflet');
+
+            return $output;
+        }
+        else { 
+            // Default condition assumes a post meta tag search by $data value
+            return $post_meta[$data][0]; 
         }
     }
     add_shortcode('dl', 'doppler_shortcode');
--- public/class-public.php
+++ public/class-public.php
@@ -10,6 +10,7 @@ class Doppler_Locator_Public {
 	public function enqueue_styles() {
 		// Register stylesheets
 		wp_register_style('stylesheet', plugin_dir_url(__FILE__) . 'assets/css/stylesheet.css');
+		wp_register_style('leaflet', plugin_dir_url(__FILE__) . 'assets/css/leaflet.css'); // Enqueue is in the shortcode
 
 		// Enqueue stylesheets
 		wp_enqueue_style('stylesheet');
@@ -17,7 +18,8 @@ class Doppler_Locator_Public {
 
 	public function enqueue_scripts() {
 		// Register scripts
-		wp_register_script('scripts', plugin_dir_url(__FILE__) . 'assets/js/scripts.js', array( 'jquery' ));
+		wp_register_script('scripts', plugin_dir_url(__FILE__) . 'assets/js/scripts.js');
+		wp_register_script('leaflet', plugin_dir_url(__FILE__) . 'assets/js/leaflet.js'); // Enqueue is in the shortcode
 
 		// Enqueue scripts
 		wp_enqueue_script('scripts');
